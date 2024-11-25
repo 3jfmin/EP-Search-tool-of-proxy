e6e1b3a833826d2815383fae03e0137c9d9a2363
3jfmin <185140062+3jfmin@users.noreply.github.com>
2024-11-25 21:24:23 +0900

    Update google-proxy.js

--- pages/api/google-proxy.js
+++ pages/api/google-proxy.js
@@ -3,12 +3,12 @@ import httpProxy from 'http-proxy';
 const proxy = httpProxy.createProxyServer();
 
 export default function handler(req, res) {
-  req.url = req.url.replace('/api/google-proxy', ''); // パスの調整
+  req.url = ''; // Googleのホームページをプロキシ
   proxy.web(req, res, { target: 'https://www.google.com', changeOrigin: true });
 }
 
 export const config = {
   api: {
-    bodyParser: false, // バイナリリクエストを許可
+    bodyParser: false, // リクエストのバイナリ処理を許可
   },
 };
