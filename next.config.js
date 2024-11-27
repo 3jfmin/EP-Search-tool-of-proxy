08e7f00735915b2ae7bc46e315e1a83c8157c1b9
3jfmin <185140062+3jfmin@users.noreply.github.com>
2024-11-27 20:32:17 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/youtube/:path*',
+        destination: '/api/youtube-proxy/:path*', // YouTubeプロキシ
+      },
+      {
         source: '/:path*',
-        destination: '/api/google-proxy/:path*', // 全てのリクエストをプロキシへ
+        destination: '/api/google-proxy/:path*', // Googleプロキシ
       },
     ];
   },
